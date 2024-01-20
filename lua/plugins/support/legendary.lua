b3a1f80b4d97131142ca6572d666c8206714a6c6
sin1111yi <sin1111yi@foxmail.com>
2024-01-21 04:22:28 +0800

    add legendary extensions

--- lua/plugins/support/legendary.lua
+++ lua/plugins/support/legendary.lua
@@ -5,6 +5,19 @@ M =
     'mrjones2014/legendary.nvim',
     priority = 10000,
     lazy = false,
+    config = function ()
+        require("legendary").setup({
+            extensions = {
+                which_key = {
+                    auto_register = true,
+                    do_binding = false,
+                    use_groups = false,
+                },
+                nvim_tree = true,
+                lazy_nvim = true,
+            }
+        })
+    end
 }
 
 return M
