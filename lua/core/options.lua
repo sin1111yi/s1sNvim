53b827b4c53c6e9ac576e09f265840de9142958c
sin1111yi <sin1111yi@foxmail.com>
2024-02-19 21:52:58 +0800

    load options adjust about netrw

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,10 +5,6 @@ vim.g.autoformat = true
 
 vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 
--- set loaded flags to 1 to disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 local opt = vim.opt
 
 opt.autowrite = true           -- Enable auto write
@@ -53,7 +49,7 @@ opt.updatetime = 200               -- Save swap file and trigger CursorHold
 opt.virtualedit = "block"          -- Allow cursor to move where there is no text in visual block mode
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5                -- Minimum window width
-opt.wrap = false                   -- Disable line wrap
+opt.wrap = true                    -- Disable line wrap
 opt.fillchars = {
     foldopen = "",
     foldclose = "",
