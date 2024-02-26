7d8c13c2a0f429b4260da8859391e62a98f9c2d6
sin1111yi <sin1111yi@foxmail.com>
2024-02-26 23:13:11 +0800

    add keymaps for neotree

--- lua/plugins/ui/neotree.lua
+++ lua/plugins/ui/neotree.lua
@@ -96,6 +96,41 @@ M = {
                     local path = node:get_id()
                     vim.fn.setreg("+", path, "c")
                 end,
+                ["<2-LeftMouse>"] = "open",
+                ["<cr>"] = "open",
+                ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+                ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+                -- Read `# Preview Mode` for more information
+                ["l"] = "focus_preview",
+                ["S"] = "split_with_window_picker",
+                ["s"] = "vsplit_with_window_picker",
+                ["t"] = "open_tabnew",
+                ["w"] = "open_with_window_picker",
+                ["C"] = "close_node",
+                ["z"] = "close_all_nodes",
+                ["Z"] = "expand_all_nodes",
+                ["a"] = {
+                    "add",
+                    -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+                    -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                    config = {
+                        show_path = "none" -- "none", "relative", "absolute"
+                    }
+                },
+                ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+                ["d"] = "delete",
+                ["r"] = "rename",
+                ["y"] = "copy_to_clipboard",
+                ["x"] = "cut_to_clipboard",
+                ["p"] = "paste_from_clipboard",
+                ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+                ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                ["q"] = "close_window",
+                ["R"] = "refresh",
+                ["?"] = "show_help",
+                ["<"] = "prev_source",
+                [">"] = "next_source",
+                ["i"] = "show_file_details",
             },
         },
         default_component_configs = {
