fcf60b25fa9b6d4dc2a1f2fafef1d68ff0719807
sin1111yi <sin1111yi@foxmail.com>
2024-03-01 02:35:02 +0800

    support markdown preview

---
+++ lua/plugins/extra/markdown.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M = {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    }
+}
+
+return M
